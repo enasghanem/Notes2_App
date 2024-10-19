f07197adcc29ce21486864e88932d6d6611dd4f5
Enas Ghanem <enasghanem151@gmail.com>
2024-10-19 11:09:49 +0300

    Add Hive

--- lib/Models/NotesModels.dart
+++ lib/Models/NotesModels.dart
@@ -1,12 +1,25 @@
-class NotesModel {
+import 'package:hive/hive.dart';
+
+part 'NotesModels.g.dart'; // تأكد من أن الامتداد صحيح
+
+@HiveType(typeId: 0) // typeAdapter
+class NotesModel extends HiveObject {
+  @HiveField(0)
   final String title;
+
+  @HiveField(1)
   final String subtitle;
+
+  @HiveField(2)
   final String date;
-  final int coloe;
+
+  @HiveField(3)
+  final int coloe; // قد ترغب في تصحيح اسم هذا المتغير إلى "color"
+
   NotesModel({
     required this.title,
     required this.subtitle,
     required this.date,
-    required this.coloe,
+    required this.coloe, // تأكد من تعديل الاسم هنا أيضًا إذا صححته
   });
 }
